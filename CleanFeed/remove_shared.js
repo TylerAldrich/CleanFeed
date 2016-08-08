88ac7fbbc26688de45e2271343155cab6573a315
Tyler Aldrich <aldrich.ty@husky.neu.edu>
2016-08-08 19:25:59 -0400

    remove console.logs

--- CleanFeed/remove_shared.js
+++ CleanFeed/remove_shared.js
@@ -1,10 +1,8 @@
 function hideShit() {
-	console.log("Running");
 	$("div:contains('shared')").each(function() {
 		var $this = $(this);
 		if ($this.hasClass("userContentWrapper")) {
 			$this.hide();
-			console.log("Hid somehting");
 		}
 	});
 }
